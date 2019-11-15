a5152c5e861dd56203a1982a1e9b8bc7e1bb4c88
YuchenY <52902570+YvchenY@users.noreply.github.com>
2019-11-15 15:41:05 -0700

    Add files via upload

---
+++ P1C-Yuchen/P1cYuchen.java
@@ -0,0 +1,15 @@
+public class P1cYuchen
+{
+    public static void main (String[] args)
+    {
+        int myInt=5;
+        if (myInt/2==0)
+        {
+            System.out.print(myInt+" is an even number.");
+        }
+        else 
+        {
+            System.out.print(myInt+" is an odd number.");
+        }
+    }
+}
